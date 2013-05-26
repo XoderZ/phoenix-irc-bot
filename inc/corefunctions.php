468b31eb5d6bf20de4f1150fa94c475cedd10b59
Jackster35 <jacksparrow320@gmail.com>
2013-05-26 18:02:14 +0200

    Made it look nicer

--- inc/corefunctions.php
+++ inc/corefunctions.php
@@ -6,14 +6,15 @@
 // *      http://code.google.com/p/phoenix-irc/bot      *
 // *                                                    *
 // ******************************************************
-function SendCommand ($cmd) 
-{ 
+function SendCommand($cmd)
+{
     global $server; //Extends our $server array to this function 
     @fwrite($server['SOCKET'], $cmd, strlen($cmd)); //sends the command to the server 
     echo "[SEND] $cmd"; //displays it on the screen 
 }
 
-function begins_with($haystack, $needle) {
+function begins_with($haystack, $needle)
+{
     return strpos($haystack, $needle) === 0;
 }
 ?>
