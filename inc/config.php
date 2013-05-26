a1eb859149335f2789392926ca04cf6f1a698e44
Jackster35 <jacksparrow320@gmail.com>
2013-05-26 18:01:35 +0200

    Made it look nicer

--- inc/config.php
+++ inc/config.php
@@ -6,22 +6,22 @@
 // *      http://code.google.com/p/phoenix-irc/bot      *
 // *                                                    *
 // ******************************************************
-$nickname = "PhoenixBot"; // nickname to use(REQUIRED)
-$ident = "Phoenix"; // ident to use (REQUIRED)
-$realname = "Phoenix IRC bot"; // Realname (REQUIRED)
-$quitmessage = "PhoenixIRC bot has been shutdown, oh noes.";  // Quit message, only works when using [PREFIX]quit (REQUIRED)
-$prefix = "."; //Command prefix (REQUIRED)
+$nickname    = "PhoenixBot"; // nickname to use(REQUIRED)
+$ident       = "Phoenix"; // ident to use (REQUIRED)
+$realname    = "Phoenix IRC bot"; // Realname (REQUIRED)
+$quitmessage = "PhoenixIRC bot has been shutdown, oh noes."; // Quit message, only works when using [PREFIX]quit (REQUIRED)
+$prefix      = "."; //Command prefix (REQUIRED)
 
 $ircserver = "irc.kottnet.net"; //  RC server you want to connect to. (REQUIRED)
-$password = ""; // Put server password (OPTIONAL)
-$port = "6667"; // Port (REQUIRED)
+$password  = ""; // Put server password (OPTIONAL)
+$port      = "6667"; // Port (REQUIRED)
 
 $channels = "#phoenixircbot"; // Channels to join (OPTIONAL)
 $nickserv = ""; // NickServ password (OPTIONAL)
-$version = "0.2.2";
+$version  = "0.2.2";
 
 // DO NOT TOUCH BELOW THIS LINE, IF YOU DO, YOU'LL VOID YOUR NON-EXISITING WARRANTY, and right on support...
 
-$new_version = file_get_contents("http://phoenix-irc-bot.googlecode.com/hg/COMMIT-ONLY-FOLDER/version.txt"); // Version, obviously, still dont touch it.
+$new_version  = file_get_contents("http://phoenix-irc-bot.googlecode.com/hg/COMMIT-ONLY-FOLDER/version.txt"); // Version, obviously, still dont touch it.
 $link_version = file_get_contents("http://phoenix-irc-bot.googlecode.com/hg/COMMIT-ONLY-FOLDER/link.txt"); // Version link, obviously, still dont touch it.
 ?>
