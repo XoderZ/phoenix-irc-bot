81c2b04626c7ee9fcb486479c4f5a07969ee91a7
Jackster35 <jacksparrow320@gmail.com>
2014-05-13 12:02:07 +0100

    Update admins.php

--- inc/admins.php
+++ inc/admins.php
@@ -1,4 +1,13 @@
 <?php
+
+// ******************************************************
+// *                  Phoenix Bot                       *
+// *        Coded by Jackster35 and xBytez              *
+// *                                                    *
+// *      https://github.com/XoderZ/phoenix-irc-bot/    *
+// *                                                    *
+// ******************************************************
+
 $admins = array(
     "EpTic!quasselcor@eptic.me",
     "xBytez!quasselcor@127.0.0.1"
