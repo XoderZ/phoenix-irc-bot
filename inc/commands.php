48af3de8ef3af1cc96ebbbd1960ad6fdd5adb9df
Jackster35 <jacksparrow320@gmail.com>
2014-05-22 08:20:29 +0100

    Stuff...

--- inc/commands.php
+++ inc/commands.php
@@ -1,6 +1,8 @@
 <?php
 //Please use brokencommands.php for now. :)
 
+global $IRC;
+
 if (strpos($IRC->buffer, $prefix . "permission")) {
 	$host = $IRC->getHost($IRC->buffer);
 	if (in_array($host, $admins)) {
