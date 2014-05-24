0b0ea9bd5fc0b0c63f91b302eeb626e4162ae550
Jackster35 <jacksparrow320@gmail.com>
2014-05-24 20:53:01 +0100

    Moved the function.

--- mods/tail.php
+++ mods/tail.php
@@ -6,28 +6,6 @@ global $IRC;
 //Do not touch unless you know what you're doing
 
 if ($tail_enabled == true) {
-    function follow($file)
-    {
-        $size = 0;
-        while (true) {
-            clearstatcache();
-            $currentSize = filesize($file);
-            if ($size == $currentSize) {
-                usleep(100);
-                continue;
-            }
-            
-            $fh = fopen($file, "r");
-            fseek($fh, $size);
-            
-            while ($d = fgets($fh)) {
-                $IRC->send("PRIVMSG " . $tail_channel . " :[".$file."]" . $d . "\r\n");
-            }
-            
-            fclose($fh);
-            $size = $currentSize;
-        }
-    }
     $pid = pcntl_fork();
     if ($pid == -1) {
         echo "Forking for tail module failed...\r\n";
@@ -36,6 +14,28 @@ if ($tail_enabled == true) {
         //Parent
     } else {
         //Child
+        function follow($file)
+        {
+            $size = 0;
+            while (true) {
+                clearstatcache();
+                $currentSize = filesize($file);
+                if ($size == $currentSize) {
+                    usleep(100);
+                    continue;
+                }
+                
+                $fh = fopen($file, "r");
+                fseek($fh, $size);
+                
+                while ($d = fgets($fh)) {
+                    $IRC->send("PRIVMSG " . $tail_channel . " :[" . $file . "]" . $d . "\r\n");
+                }
+                
+                fclose($fh);
+                $size = $currentSize;
+            }
+        }
         follow($tail_file);
     }
 }
