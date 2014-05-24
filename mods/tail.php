41c6b4dff1d6ca6a81b7a20abf6c2ff64e8fa577
Jackster35 <jacksparrow320@gmail.com>
2014-05-24 21:59:34 +0100

    Trying new way :/

--- mods/tail.php
+++ mods/tail.php
@@ -13,26 +13,49 @@ if ($tail_enabled == true) {
     } else if ($pid) {
         //Parent
     } else {
-        //Child
-        $size = filesize($tail_file);
-        while (true) {
-            clearstatcache();
-            $currentSize = filesize($tail_file);
-            if ($size == $currentSize) {
-                usleep(100);
-                continue;
-            }
+        // Open the file
+        $f = fopen($tail_file, "rb");
+        
+        // Jump to last character
+        fseek($f, -1, SEEK_END);
+        
+        // Read it and adjust line number if necessary
+        // (Otherwise the result would be wrong if file doesn't end with a blank line)
+        if (fread($f, 1) != "\n")
+            $lines -= 1;
+        
+        // Start reading
+        $output = '';
+        $chunk  = '';
+        
+        // While we would like more
+        while (ftell($f) > 0 && $lines >= 0) {
+            // Figure out how far back we should jump
+            $seek = min(ftell($f), $buffer);
             
-            $fh = fopen($tail_file, "r");
-            fseek($fh, $size);
+            // Do the jump (backwards, relative to where we are)
+            fseek($f, -$seek, SEEK_CUR);
             
-            while ($d = fgets($fh)) {
-                $IRC->send("PRIVMSG " . $tail_channel . " :[" . $tail_file . "]" . $d . "\r\n");
-            }
+            // Read a chunk and prepend it to our output
+            $output = ($chunk = fread($f, $seek)) . $output;
             
-            fclose($fh);
-            $size = $currentSize;
+            // Jump back to where we started reading
+            fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
+            
+            // Decrease our line counter
+            $lines -= substr_count($chunk, "\n");
         }
+        
+        // While we have too many lines
+        // (Because of buffer size we might have read too many)
+        while ($lines++ < 0) {
+            // Find first newline and remove all text before that
+            $output = substr($output, strpos($output, "\n") + 1);
+        }
+        
+        // Close file and return
+        fclose($f);
+        $IRC->send("PRIVMSG " . $tail_channel . " :[" . $tail_file . "] " . $d . "\r\n");
     }
 }
 ?>
