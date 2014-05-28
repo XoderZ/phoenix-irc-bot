c9c9fc113a86df0c0fa7129e792b603ffced3bfe
xBytez <me@xbytez.eu>
2014-05-28 23:13:21 +0200

    Using the main bot channel

--- inc/config.php
+++ inc/config.php
@@ -18,21 +18,21 @@ $nickserv	 = ""; // NickServ password (OPTIONAL)
 $ircserver	= "192.81.220.223"; //  RC server you want to connect to. (REQUIRED)
 $password	= ""; // Put server password (OPTIONAL)
 $port		= "6667"; // Port (REQUIRED)
-$channels	= "#xBytez"; // Channels to join (OPTIONAL)
+$channels	= "#phoenix-irc-bot"; // Channels to join (OPTIONAL)
 $version	= "1.0";
 //Module configs
 $dangerous_functions = true;
 
 //Shoutcast config - This module has been tested only on 1.9.8
-$sc_enabled	= false; //Is this module enabled on the bot? False = No, True = Yes
+$sc_enabled	= true; //Is this module enabled on the bot? False = No, True = Yes
 $sc_ip		= "69.46.88.20"; //Can also be a domain
 $sc_port	= "80"; // Port
-$sc_channel	= "#jack"; //Channel to send Shoutcast data to.
+$sc_channel	= "#phoenix-irc-bot"; //Channel to send Shoutcast data to.
 //END
 
 //Tail config
 $tail_enabled = true;
 $tail_file = "/root/test.txt";
-$tail_channel = "#xBytez";
+$tail_channel = "#phoenix-irc-bot";
 //END
 ?>
