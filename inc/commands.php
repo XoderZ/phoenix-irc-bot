39087168aa3b9e1f929268b31f876fb6e528de26
xBytez <me@xbytez.eu>
2014-05-28 22:15:22 +0200

    DEBUG! :D

--- inc/commands.php
+++ inc/commands.php
@@ -63,7 +63,9 @@ if (strpos($IRC->buffer, $prefix . "eval")) {
 	$IRC->parseData($IRC->buffer, $prefix . "eval");
 	if($dangerous_functions == true) {
 		if (in_array($IRC->host, $admins)) {
+			print_r("[DEBUG-EVAL-ARGS] ".$IRC->args."\r\n");
 			$str = substr($IRC->args, 1);
+			print_r("[DEBUG-EVAL-ARGS] ".$str."\r\n");
 			ob_start();
 			eval($str);
 			$eval = ob_get_contents();
