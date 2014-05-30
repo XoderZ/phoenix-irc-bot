3483005bbeb644e8b1c815523501a328775e75f3
Jackster35 <jacksparrow320@gmail.com>
2014-05-30 20:21:43 +0100

    Changed some stuff before merge.

--- inc/config.php
+++ inc/config.php
@@ -19,20 +19,20 @@ $ircserver	= "192.81.220.223"; //  RC server you want to connect to. (REQUIRED)
 $password	= ""; // Put server password (OPTIONAL)
 $port		= "6667"; // Port (REQUIRED)
 $channels	= "#phoenix-irc-bot"; // Channels to join (OPTIONAL)
-$version	= "1.2";
+$version	= "1.3";
 
 //Module configs
-$dangerous_functions = true;
+$dangerous_functions = false;
 
 //Shoutcast config - This module has been tested only on 1.9.8
-$sc_enabled	= true; //Is this module enabled on the bot? False = No, True = Yes
+$sc_enabled	= false; //Is this module enabled on the bot? False = No, True = Yes
 $sc_ip		= "69.46.88.20"; //Can also be a domain
 $sc_port	= "80"; // Port
 $sc_channel	= "#phoenix-irc-bot"; //Channel to send Shoutcast data to.
 //END
 
 //Tail config
-$tail_enabled = true;
+$tail_enabled = false;
 $tail_file = "tail.txt";
 $tail_channel = "#phoenix-irc-bot";
 //END
