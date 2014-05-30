fe56e683e7d0cd2fac192299e9a141b0af593ef9
Jackster35 <jacksparrow320@gmail.com>
2014-05-31 00:09:16 +0100

    Don't need that :3

--- mods/tail.php
+++ mods/tail.php
@@ -4,45 +4,43 @@
 global $IRC;
 
 //Do not touch unless you know what you're doing
-if (strpos($IRC->buffer, "376")) {
-    if ($tail_enabled == true) {
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            echo ("This module does NOT work for Windows.");
+if ($tail_enabled == true) {
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        echo ("This module does NOT work for Windows.");
+    } else {
+        echo "[MODULES] Tail module started...\r\n";
+        $pid = pcntl_fork();
+        if ($pid == -1) {
+            echo "Forking for tail module failed...\r\n";
+            exit(1);
+        } else if ($pid) {
+            //Parent
         } else {
-            echo "[MODULES] Tail module started...\r\n";
-            $pid = pcntl_fork();
-            if ($pid == -1) {
-                echo "Forking for tail module failed...\r\n";
-                exit(1);
-            } else if ($pid) {
-                //Parent
+            //Child
+            $IRC->send("JOIN :" . $tail_channel . "\r\n");
+            if (!file_exists($tail_file)) {
+                echo ("[Tail] File to tail does NOT exist. Please change the file you want to tail or create " . $tail_file . "\r\n");
+                $IRC->send("PRIVMSG " . $tail_channel . " :[Tail] File to tail does NOT exist. Please change the file you want to tail or create " . $tail_file . "\r\n");
             } else {
-                //Child
-                $IRC->send("JOIN :" . $tail_channel . "\r\n");
-                if (!file_exists($tail_file)) {
-                    echo ("[Tail] File to tail does NOT exist. Please change the file you want to tail or create " . $tail_file . "\r\n");
-                    $IRC->send("PRIVMSG " . $tail_channel . " :[Tail] File to tail does NOT exist. Please change the file you want to tail or create " . $tail_file . "\r\n");
-                } else {
-                    $IRC->send("PRIVMSG " . $tail_channel . " :[Tail] Module loaded.\r\n");
-                    $size = filesize($tail_file);
-                    while (true) {
-                        clearstatcache();
-                        $currentSize = filesize($tail_file);
-                        if ($size == $currentSize) {
-                            usleep(100);
-                            continue;
-                        }
-                        
-                        $fh = fopen($tail_file, "r");
-                        fseek($fh, $size);
-                        
-                        while ($d = fgets($fh)) {
-                            $IRC->send("PRIVMSG " . $tail_channel . " :[" . $tail_file . "] " . $d . "\r\n");
-                        }
-                        
-                        fclose($fh);
-                        $size = $currentSize;
+                $IRC->send("PRIVMSG " . $tail_channel . " :[Tail] Module loaded.\r\n");
+                $size = filesize($tail_file);
+                while (true) {
+                    clearstatcache();
+                    $currentSize = filesize($tail_file);
+                    if ($size == $currentSize) {
+                        usleep(100);
+                        continue;
                     }
+                    
+                    $fh = fopen($tail_file, "r");
+                    fseek($fh, $size);
+                    
+                    while ($d = fgets($fh)) {
+                        $IRC->send("PRIVMSG " . $tail_channel . " :[" . $tail_file . "] " . $d . "\r\n");
+                    }
+                    
+                    fclose($fh);
+                    $size = $currentSize;
                 }
             }
         }
