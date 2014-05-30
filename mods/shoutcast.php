01fce6eab3ba0da7ebb14a5c51ab74a75c5fd15e
Jackster35 <jacksparrow320@gmail.com>
2014-05-30 11:42:13 +0100

    Made it clear I meant shoutcast.

--- mods/shoutcast.php
+++ mods/shoutcast.php
@@ -1,6 +1,6 @@
 <?php
 
-//Shoutcast module created by TheEpTic & xBytez - This module has been tested only on 1.9.8
+//Shoutcast module created by TheEpTic & xBytez - This module has been tested only on shoutcast 1.9.8
 global $IRC;
 
 //Do not touch unless you know what you're doing
